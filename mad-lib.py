5d2e3564f1e7623f12f7df14195e7326c7154957
deweyda334 <153093259+deweyda334@users.noreply.github.com>
2024-09-16 13:56:14 -0400

    Add files via upload

---
+++ mad-lib.py
@@ -0,0 +1,25 @@
+noun = input("Noun: ")
+verb = input("Verb: ")
+noun2 = input("Noun (plural): ")
+noun3 = input("Noun (plural): ")
+verb2 = input("Verb: ")
+noun4 = input("Noun: ")
+
+txt = "Hello my applesause"
+
+x1 = txt.upper()
+
+txt = "Hello my DEWEY"
+
+x2 = txt.lower()
+
+
+
+
+
+madlibs = f"Hey! Welcome to my {noun}. Here, we {verb} about {noun2} and we have fun with {noun3}. Make sure you {verb2} to my {noun4}."
+
+
+print(madlibs)
+print(x1)
+print(x2)
